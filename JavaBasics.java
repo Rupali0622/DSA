c162010ab6e6d324f4fe17b21be04c637d7a29a4
Rupali <rupalichauhan022@gmail.com>
2024-01-13 17:27:02 +0530

    First DSA Commit

---
+++ JavaBasics.java
@@ -0,0 +1,8 @@
+public class JavaBasics{
+    public static void main(String[] args) {
+        int number1=10;
+        int number2=5;
+        int sum=number1+number2;
+        System.out.println("Sum = "+sum);
+    }
+}
