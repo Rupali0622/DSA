1b1e78d872d553988e53712b7e8ede4b98071c76
Rupali <rupalichauhan022@gmail.com>
2024-01-13 21:10:13 +0530

    pattern

--- Basics/JavaBasics.java
+++ Basics/JavaBasics.java
@@ -1,8 +1,24 @@
 public class JavaBasics{
     public static void main(String[] args) {
         int number1=10;
-        int number2=5;
-        int sum=number1+number2;
-        System.out.println("Sum = "+sum);
+        // int number2=5;
+        // int sum=number1+number2;
+        // System.out.println("Sum = "+sum);
+
+        //upper part
+        for(int i=1; i<=number1; i++){
+            for(int j=1; j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        //lower part
+        for(int i=number1-1; i>=1; i--){
+            for(int j=1; j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
