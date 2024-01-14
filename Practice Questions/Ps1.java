9fbd96acd9ff6ce42ed97c1ab44c644645444e89
Rupali <rupalichauhan022@gmail.com>
2024-01-14 23:40:05 +0530

    type conversion

---
+++ Practice Questions/Ps1.java
@@ -0,0 +1,24 @@
+public class Ps1{
+    public static void main(String[] args) {
+    
+    /*float pencil=10.5f;
+    float pen=10.0f;
+    float eraser=5.3f;
+
+    float total_cost=pencil+pen+eraser;
+    System.out.println(total_cost);
+    float new_cost =(float) .18+total_cost;
+    System.out.println(new_cost);*/
+
+        byte b = 4;
+        char c = 'a';
+        short s = 512;
+        int i = 1000;
+        float f = 3.14f;
+        double d = 99.9954;
+
+        double result = (f * b)+(i % c)-(d*s);
+        System.out.println(result);
+
+    }
+}
