0c9afff2d6aaea1badcb0af6f5fee2ebb10cd388
Rupali <rupalichauhan022@gmail.com>
2024-01-14 16:45:36 +0530

    Java output

---
+++ Basics/OutputValue.java
@@ -0,0 +1,16 @@
+public class OutputValue {
+    public static void main(String[] args) {
+
+        //Using print (Here print is a function)
+        System.out.print("Hello World!");
+        System.out.print("Hello World! ");
+
+        //Using println 
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+
+        //Using \n
+        System.out.print("Hello World!\n");
+        System.out.print("Hello World!\n");
+    }
+}
