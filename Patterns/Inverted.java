4620a0f7ee17031fb5961732a9c192135a3f6836
Rupali <rupalichauhan022@gmail.com>
2024-01-17 10:43:53 +0530

    Quiz

--- Patterns/Inverted.java
+++ Patterns/Inverted.java
@@ -2,17 +2,17 @@ package Patterns;
 
 import java.util.Scanner;
 
-public class Star {
+public class Inverted {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.print("Number of lines in Pattern= ");
-        int lines=sc.nextInt();
-        for(int i=1; i<=lines; i++){
-            for(int j=1; j<=i; j++){
-                System.out.print(" *");
+        int n=sc.nextInt();
+
+        for(int i=1; i<=n; i++){
+            for(int j=n; j>=1; j--){
+                System.out.print("* ");
             }
             System.out.println();
         }
-
     }
 }
---
+++ Practice Questions/Ps15Quiz.java
@@ -0,0 +1,17 @@
+public class Ps15Quiz {
+    
+        
+    Ps15Quiz(){
+            System.out.print("ONE, ");
+    }
+        }
+    class B extends Ps15Quiz {
+            B() {
+            System.out.print("TWO");
+            }
+    public static void main(String[] args) {
+            new B();
+    }
+    }
+    
+
